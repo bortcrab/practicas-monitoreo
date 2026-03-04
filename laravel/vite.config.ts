977eb55e34b0a6e09dee6af49145f862925b9112
Diego Valenzuela <bortcrab@gmail.com>
2026-03-04 23:20:12 +0000

    Creé las migraciones del proyecto de Laravel

--- laravel/vite.config.ts
+++ laravel/vite.config.ts
@@ -5,6 +5,12 @@ import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    server: {
+      host: true,
+      hmr: {
+        host: '192.168.1.154'
+      }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
